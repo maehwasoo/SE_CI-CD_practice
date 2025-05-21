b25122ab624f6de2d1d4d5441f7b61f2c612d0d5
soeun <soeunp234@gmail.com>
2025-05-21 11:51:46 +0900

    test

--- Test1/src/student/StudentManager.java
+++ Test1/src/student/StudentManager.java
@@ -22,7 +22,7 @@ public class StudentManager {
         students.remove(name);
     }
 
-    // 학생 유무 확인222
+    // 학생 유무 확인
     public boolean hasStudent(String name) {
         return students.contains(name);
     }
